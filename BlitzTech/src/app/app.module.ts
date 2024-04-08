00210224dbb8d551f0e0bbee6903866de764f5b2
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-04-08 10:37:15 -0300

    Subindo introduction

--- BlitzTech/src/app/app.module.ts
+++ BlitzTech/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
---
+++ BlitzTech/src/app/components/introduction/introduction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introduction',
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.css'
+})
+export class IntroductionComponent {
+
+}
