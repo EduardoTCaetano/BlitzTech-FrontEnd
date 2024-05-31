83691b19fb1d37f458cebdb890a00cabc5302ad7
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-05-31 17:22:37 -0300

    Subindo atualização

--- BlitzTech/src/app/app.module.ts
+++ BlitzTech/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { CardsComponent } from './components/cards/cards.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
