93c2c7775539f488f50d000bd8cb4c59af8abc62
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-06-18 11:31:34 -0300

    Subindo carrosel propaganda

--- BlitzTech/src/app/components/advertising/advertising.component.ts
+++ BlitzTech/src/app/components/advertising/advertising.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-advertising',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './advertising.component.css'
 })
 export class AdvertisingComponent {
+  @ViewChild('carouselInner') carouselInner!: ElementRef;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 4000); // Altera o slide a cada 3 segundos (3000 milissegundos)
+  }
+
+  nextSlide() {
+    const carouselInner = this.carouselInner.nativeElement as HTMLElement;
+    const activeItem = carouselInner.querySelector('.carousel-item.active');
+    if (activeItem) {
+      activeItem.classList.remove('active');
+      const nextItem = activeItem.nextElementSibling || carouselInner.firstElementChild;
+      if (nextItem) {
+        nextItem.classList.add('active');
+      }
+    }
+  }
 
 }
+
+
+
