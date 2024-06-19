4829864bbf8888eeaa7746c2e915d144c3029885
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-06-18 21:13:14 -0300

    Arrumando propaganda

--- BlitzTech/src/app/components/advertising/advertising.component.ts
+++ BlitzTech/src/app/components/advertising/advertising.component.ts
@@ -6,25 +6,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrl: './advertising.component.css'
 })
 export class AdvertisingComponent {
-  @ViewChild('carouselInner') carouselInner!: ElementRef;
 
-  ngOnInit() {
-    setInterval(() => {
-      this.nextSlide();
-    }, 4000); // Altera o slide a cada 3 segundos (3000 milissegundos)
-  }
-
-  nextSlide() {
-    const carouselInner = this.carouselInner.nativeElement as HTMLElement;
-    const activeItem = carouselInner.querySelector('.carousel-item.active');
-    if (activeItem) {
-      activeItem.classList.remove('active');
-      const nextItem = activeItem.nextElementSibling || carouselInner.firstElementChild;
-      if (nextItem) {
-        nextItem.classList.add('active');
-      }
-    }
-  }
 
 }
 
